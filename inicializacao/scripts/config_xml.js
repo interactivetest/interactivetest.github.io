38b8d8069ec6bf5beb5131b67a7e5970251f0433
PedroKazi <69813674+PedroKazi@users.noreply.github.com>
2020-10-08 13:52:06 -0300

    site 1

---
+++ inicializacao/scripts/config_xml.js
@@ -0,0 +1,46 @@
+var TSC = TSC || {};
+
+TSC.embedded_config_xml = '<x:xmpmeta tsc:version="2.0.1" xmlns:x="adobe:ns:meta/" xmlns:tsc="http://www.techsmith.com/xmp/tsc/">\
+   <rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:xmp="http://ns.adobe.com/xap/1.0/" xmlns:xmpDM="http://ns.adobe.com/xmp/1.0/DynamicMedia/" xmlns:xmpG="http://ns.adobe.com/xap/1.0/g/" xmlns:xmpMM="http://ns.adobe.com/xap/1.0/mm/" xmlns:tscDM="http://www.techsmith.com/xmp/tscDM/" xmlns:tscIQ="http://www.techsmith.com/xmp/tscIQ/" xmlns:tscHS="http://www.techsmith.com/xmp/tscHS/" xmlns:stDim="http://ns.adobe.com/xap/1.0/sType/Dimensions#" xmlns:stFnt="http://ns.adobe.com/xap/1.0/sType/Font#" xmlns:exif="http://ns.adobe.com/exif/1.0" xmlns:dc="http://purl.org/dc/elements/1.1/">\
+      <rdf:Description dc:date="2020-10-08 01:38:56 " dc:source="Camtasia,20.0.8,ptb" dc:title="projeto teste 1" tscDM:firstFrame="projeto_teste_1_First_Frame.png" tscDM:originId="4CE0DD98-DF98-4235-AF17-5A0C30EAC201" tscDM:project="projeto teste 1">\
+         <xmpDM:duration xmpDM:scale="1/1000" xmpDM:value="368700"/>\
+         <xmpDM:videoFrameSize stDim:unit="pixel" stDim:h="720" stDim:w="1280"/>\
+         <tsc:langName>\
+            <rdf:Bag>\
+               <rdf:li xml:lang="pt-BR">Portuguese</rdf:li></rdf:Bag>\
+         </tsc:langName>\
+         <xmpDM:Tracks>\
+            <rdf:Bag>\
+               <rdf:li>\
+                  <rdf:Description xmpDM:trackType="Hotspot" xmpDM:frameRate="f1000" xmpDM:trackName="Hotspots">\
+                     <xmpDM:markers>\
+                        <rdf:Seq>\
+                           <rdf:li><rdf:Description xmpDM:label="1" xmpDM:startTime="332370" xmpDM:duration="12730" tscDM:boundingPoly="0,0;1280,0;1280,720;0,720;" tscDM:rotate="0.000000" tscHS:pause="1" xmpDM:location="https://interactivetest.github.io/AREARESTRITA/index.html" tscHS:newWindow="1"/></rdf:li><rdf:li><rdf:Description xmpDM:label="2" xmpDM:startTime="87070" xmpDM:duration="12730" tscDM:boundingPoly="0,0;1280,0;1280,720;0,720;" tscDM:rotate="0.000000" tscHS:pause="1" xmpDM:location="https://gbreschau.wixsite.com/website" tscHS:newWindow="0"/></rdf:li><rdf:li><rdf:Description xmpDM:label="3" xmpDM:startTime="78170" xmpDM:duration="8830" tscDM:boundingPoly="175,414;441,414;441,581;175,581;" tscDM:rotate="0.000000" tscHS:pause="1" tscHS:jumpTime="161667"/></rdf:li><rdf:li><rdf:Description xmpDM:label="4" xmpDM:startTime="136800" xmpDM:duration="8830" tscDM:boundingPoly="175,414;441,414;441,581;175,581;" tscDM:rotate="0.000000" tscHS:pause="1" tscHS:jumpTime="265900"/></rdf:li><rdf:li><rdf:Description xmpDM:label="5" xmpDM:startTime="233830" xmpDM:duration="16770" tscDM:boundingPoly="99,414;498,414;498,581;99,581;" tscDM:rotate="0.000000" tscHS:pause="1" xmpDM:location="https://interactivetest.github.io/AREARESTRITA/index.html" tscHS:newWindow="0"/></rdf:li><rdf:li><rdf:Description xmpDM:label="6" xmpDM:startTime="311330" xmpDM:duration="16770" tscDM:boundingPoly="99,414;498,414;498,581;99,581;" tscDM:rotate="0.000000" tscHS:pause="1" xmpDM:location="https://interactivetest.github.io/AREARESTRITA/index.html" tscHS:newWindow="0"/></rdf:li><rdf:li><rdf:Description xmpDM:label="7" xmpDM:startTime="364430" xmpDM:duration="4000" tscDM:boundingPoly="132,81;1148,81;1148,638;132,638;" tscDM:rotate="0.000000" tscHS:pause="1" xmpDM:location="https://gbreschau.wixsite.com/website" tscHS:newWindow="0"/></rdf:li><rdf:li><rdf:Description xmpDM:label="8" xmpDM:startTime="78170" xmpDM:duration="8830" tscDM:boundingPoly="768,414;1035,414;1035,581;768,581;" tscDM:rotate="0.000000" tscHS:pause="1" tscHS:jumpTime="99967"/></rdf:li><rdf:li><rdf:Description xmpDM:label="9" xmpDM:startTime="136800" xmpDM:duration="8830" tscDM:boundingPoly="768,414;1035,414;1035,581;768,581;" tscDM:rotate="0.000000" tscHS:pause="1" xmpDM:location="https://interactivetest.github.io/AREARESTRITA/index.html" tscHS:newWindow="0"/></rdf:li><rdf:li><rdf:Description xmpDM:label="10" xmpDM:startTime="146900" xmpDM:duration="12730" tscDM:boundingPoly="0,0;1280,0;1280,720;0,720;" tscDM:rotate="0.000000" tscHS:pause="0" tscHS:jumpTime="160033"/></rdf:li><rdf:li><rdf:Description xmpDM:label="11" xmpDM:startTime="233830" xmpDM:duration="16770" tscDM:boundingPoly="768,414;1035,414;1035,581;768,581;" tscDM:rotate="0.000000" tscHS:pause="1" tscHS:jumpTime="265733"/></rdf:li><rdf:li><rdf:Description xmpDM:label="12" xmpDM:startTime="311330" xmpDM:duration="16770" tscDM:boundingPoly="768,414;1035,414;1035,581;768,581;" tscDM:rotate="0.000000" tscHS:pause="1" tscHS:jumpTime="361500"/></rdf:li></rdf:Seq>\
+                     </xmpDM:markers>\
+                  </rdf:Description>\
+               </rdf:li>\
+            </rdf:Bag>\
+         </xmpDM:Tracks>\
+         <tscDM:controller>\
+            <rdf:Description xmpDM:name="tscplayer">\
+               <tscDM:parameters>\
+                  <rdf:Bag>\
+                     <rdf:li xmpDM:name="autohide" xmpDM:value="true"/><rdf:li xmpDM:name="autoplay" xmpDM:value="false"/><rdf:li xmpDM:name="loop" xmpDM:value="false"/><rdf:li xmpDM:name="searchable" xmpDM:value="true"/><rdf:li xmpDM:name="captionsenabled" xmpDM:value="false"/><rdf:li xmpDM:name="sidebarenabled" xmpDM:value="false"/><rdf:li xmpDM:name="unicodeenabled" xmpDM:value="false"/><rdf:li xmpDM:name="backgroundcolor" xmpDM:value="000000"/><rdf:li xmpDM:name="sidebarlocation" xmpDM:value="left"/><rdf:li xmpDM:name="endaction" xmpDM:value="stop"/><rdf:li xmpDM:name="endactionparam" xmpDM:value="true"/><rdf:li xmpDM:name="locale" xmpDM:value="pt-BR"/></rdf:Bag>\
+               </tscDM:parameters>\
+               <tscDM:controllerText>\
+                  <rdf:Bag>\
+                  </rdf:Bag>\
+               </tscDM:controllerText>\
+            </rdf:Description>\
+         </tscDM:controller>\
+         <tscDM:contentList>\
+            <rdf:Description>\
+               <tscDM:files>\
+                  <rdf:Seq>\
+                     <rdf:li xmpDM:name="0" xmpDM:value="projeto teste 1.mp4"/><rdf:li xmpDM:name="1" xmpDM:value="projeto_teste_1_First_Frame.png"/><rdf:li xmpDM:name="2" xmpDM:value="projeto_teste_1_Thumbnails.png"/></rdf:Seq>\
+               </tscDM:files>\
+            </rdf:Description>\
+         </tscDM:contentList>\
+      </rdf:Description>\
+   </rdf:RDF>\
+</x:xmpmeta>';
